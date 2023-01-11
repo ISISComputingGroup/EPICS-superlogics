70c81554adca01c8dbb47498880a18867d35595c
Nikola Roev <nikola.roev@stfc.ac.uk>
2023-01-11 15:47:42 +0000

    Add back emulator changes.

--- system_tests/lewis_emulators/superlogics/interfaces/stream_interface.py
+++ system_tests/lewis_emulators/superlogics/interfaces/stream_interface.py
@@ -1,25 +1,21 @@
-from __future__ import print_function, absolute_import, division
+from functools import partial
+
 from lewis.adapters.stream import StreamInterface
 from lewis.utils.command_builder import CmdBuilder
+from lewis.core.logging import has_log
 from lewis.utils.replies import conditional_reply
 
-if_connected = conditional_reply("connected")
-
-EXPECTED_ADDRESSES = ["01", "02"]
-
-
+@has_log
 class SuperlogicsStreamInterface(StreamInterface):
-    """
-    Stream interface for the serial port
-    """
-
-    commands = {
-        CmdBuilder("get_values").escape("#").arg("[0-9]+").build(),
-        CmdBuilder("get_version").escape("$").arg("[0-9]+").escape("F").build()
-    }
 
     in_terminator = "\r"
     out_terminator = "\r"
+
+    def __init__(self):
+        super(SuperlogicsStreamInterface, self).__init__()
+        self.commands = {
+            CmdBuilder(self.get_channel).escape("#").arg("[0-9A-F]{2}").arg("[0-7]{1}", argument_mapping=partial(int)).eos().build(),
+        }
 
     def handle_error(self, request, error):
         """
@@ -30,31 +26,8 @@ class SuperlogicsStreamInterface(StreamInterface):
             error: problem
 
         """
-        print("An error occurred at request {}: {}".format(request, error))
+        self.log.error("An error occurred at request " + repr(request) + ": " + repr(error))
 
-    @if_connected
-    def get_values(self, address):
-        """
-        Gets the values from the device
-
-        Returns: List of values, one for each connected channel
-        """
-        if address not in EXPECTED_ADDRESSES:
-            raise ValueError("Invalid address '{}'".format(address))
-
-        values = self._device.values_1 if address == "01" else self._device.values_2
-        formatted_values = map(lambda s: "+{0:.2f}".format(s), values)
-        return ",".join(formatted_values)
-
-    @if_connected
-    def get_version(self, address):
-        """
-        Get the firmware version from the device
-        :param address: the address to read the version from
-        :return: string representing the firmware version for the address
-        """
-        if address not in EXPECTED_ADDRESSES:
-            raise ValueError("Invalid address '{}'".format(address))
-
-        version = self._device.version_1 if address == "01" else self._device.version_2
-        return "!{0}{1}".format(address, version)
+    @conditional_reply("connected")
+    def get_channel(self, _, channel):
+        return f">{self.device.get_channel(channel)}"
--- system_tests/lewis_emulators/superlogics/states.py
+++ system_tests/lewis_emulators/superlogics/states.py
@@ -2,11 +2,4 @@ from lewis.core.statemachine import State
 
 
 class DefaultState(State):
-    """
-    Device is in default state.
-
-    """
-    NAME = 'Default'
-
-
-
+    pass
